179886665820ebe1d49d6b4834d700161615d6ee
Yuriy Zymlex <id@zymlex.com>
2024-03-31 10:51:47 +1000

    Test 1

---
+++ index.js
@@ -0,0 +1,32 @@
+init();
+
+function init() {
+    load_public_key();
+}
+
+function load_public_key() {
+    // const el = document.getElementById("Public_commit_key");
+    
+    // el.style.visibility = null;
+    getText();
+}
+
+function getText(){
+    let http = new XMLHttpRequest();
+    http.timeout = 10;
+    http.addEventListener("loadend", function (ev) {
+        if (http.readyState === 4 && http.status === 200) {
+            let type = http.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                console.log(http.responseText);
+            }
+        } else {
+            console.log(new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`));
+        }
+    });
+    // http.addEventListener("error", function () {});
+    // http.addEventListener("abort", handleEvent);
+    
+    http.open('GET', './82A59AD376D5A94E', true);
+    http.send(null);
+}
