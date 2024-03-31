dc32561252036f3c272d0cd5235f91cc86750bbb
Yuriy Zymlex <id@zymlex.com>
2024-03-31 10:51:47 +1000

    update 2

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+init();
+
+function init() {
+    loadPubKey();
+}
+
+function loadPubKey(){
+    let http = new XMLHttpRequest();
+    http.timeout = 10000;
+    http.addEventListener("loadend", function () {
+        if (http.readyState === 4 && http.status === 200) {
+            let type = http.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                setKeyValue(http.responseText);
+            }
+        } else {
+            setKeyValue(new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`));
+        }
+    });
+    // http.addEventListener("error", function () {});
+    // http.addEventListener("abort", handleEvent);
+    
+    http.open('GET', './82A59AD376D5A94E', true);
+    http.send(null);
+}
+
+function setKeyValue(str) {
+    const el = document.getElementById("Public_commit_key");
+    
+    el.value = str;
+    
+    el.style.visibility = null;
+}
