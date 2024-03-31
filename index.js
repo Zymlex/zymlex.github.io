f26bc559b728f95102271f897907a442c9d02087
Yuriy Zymlex <id@zymlex.com>
2024-03-31 12:01:29 +1000

    Test 7

--- index.js
+++ index.js
@@ -3,33 +3,33 @@
 init();
 
 function init() {
-    load_public_key();
+    loadPubKey();
 }
 
-function load_public_key() {
-    getText();
-}
-
-function getText(){
+function loadPubKey(){
     let http = new XMLHttpRequest();
     http.timeout = 10000;
     http.addEventListener("loadend", function (ev) {
         if (http.readyState === 4 && http.status === 200) {
             let type = http.getResponseHeader('Content-Type');
             if (type.indexOf("text") !== 1) {
-                const el = document.getElementById("Public_commit_key");
-                
-                el.value = http.responseText;
-                
-                el.style.visibility = null;
+                setKeyValue(http.responseText);
             }
         } else {
-            console.log(new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`));
+            setKeyValue(new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`));
         }
     });
     // http.addEventListener("error", function () {});
     // http.addEventListener("abort", handleEvent);
     
-    http.open('GET', './82A59AD376D5A94E', true);
+    http.open('GET', './82A59AD376D5A94E!!', true);
     http.send(null);
+}
+
+function setKeyValue(str) {
+    const el = document.getElementById("Public_commit_key");
+    
+    el.value = str;
+    
+    el.style.visibility = null;
 }
