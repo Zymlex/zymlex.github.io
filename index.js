9f2f337d133f2bc3ecf5f1ee1c56d9b7111db400
Yuriy Zymlex <id@zymlex.com>
2024-04-05 06:06:58 +1000

    update 3

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
-"use strict";
+﻿'use strict';
 
-init();
+(function init() {
+    publicKey();
+})();
 
-function init() {
-    loadPubKey();
+function publicKey() {
+    let key = getPubKey();
+    
+    let observer = new MutationObserver(mutationRecords => {
+        mutationRecords.forEach(function (v/*, index, arr*/) {
+            v.addedNodes.forEach(function (v/*, index, arr*/) {
+                if (v.nodeName === 'TEXTAREA' && v.id === "public commit key") {
+                    setKeyValue(key, v);
+                }
+            });
+            
+        });
+    });
+    
+    observer.observe(window.document, {
+        childList: true, subtree: true,
+    });
 }
 
-function loadPubKey(){
+function getPubKey() {
     let http = new XMLHttpRequest();
-    http.timeout = 10000;
-    http.addEventListener("loadend", function () {
-        if (http.readyState === 4 && http.status === 200) {
-            let type = http.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                setKeyValue(http.responseText);
-            }
-        } else {
-            setKeyValue(new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`));
-        }
+    // http.timeout = 10000;
+    // http.addEventListener('loadend', function () {});
+    http.addEventListener('error', function () {
+        return new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`);
     });
-    // http.addEventListener("error", function () {});
     // http.addEventListener("abort", handleEvent);
     
-    http.open('GET', './82A59AD376D5A94E', true);
+    http.open('GET', './pubkey', false); // will wait
     http.send(null);
+    
+    if (http.readyState === 4 && http.status === 200) {
+        let type = http.getResponseHeader('Content-Type');
+        if (type.indexOf('text') !== 1) {
+            return http.responseText;
+        }
+    } else {
+        return new Error(`Loading error.\nreadyState: ${http.readyState}\nhttp.status: ${http.status}\n`);
+    }
 }
 
-function setKeyValue(str) {
-    const el = document.getElementById("Public_commit_key");
-    
-    el.value = str;
-    
-    el.style.visibility = null;
+function setKeyValue(str, node) {
+    node.value = str;
+    node.style.visibility = null;
 }
